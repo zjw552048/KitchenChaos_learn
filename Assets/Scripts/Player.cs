84af23d915e7822768e45d8b96af8852ca3633c4
zjw-pc <>
2023-07-27 17:46:45 +0800

    为player添加检测碰撞物体无法移动逻辑

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float playerRadius= 0.7f;
+    [SerializeField] private float playerHeight = 2f;
     [SerializeField] private PlayerInput playerInput;
     private Transform playTransform;
 
@@ -14,10 +16,52 @@ public class Player : MonoBehaviour {
 
     private void Update() {
         var inputVector = playerInput.GetMovementVector2Normalized();
-        
+
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        playTransform.position += moveDir * moveSpeed * Time.deltaTime;
+        var moveDistance = moveSpeed * Time.deltaTime;
+
+        // 更新角度
         playTransform.forward = Vector3.Slerp(playTransform.forward, moveDir, rotateSpeed * Time.deltaTime);
+
+        // check move on x and y dir
+        var playerPos = playTransform.position;
+        var canMove = !Physics.CapsuleCast(
+            playerPos,
+            playerPos + Vector3.up * playerHeight,
+            playerRadius,
+            moveDir,
+            moveDistance);
+        if (!canMove) {
+            // try move on x dir
+            var moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(
+                playerPos,
+                playerPos + Vector3.up * playerHeight,
+                playerRadius,
+                moveDirX,
+                moveDistance);
+            if (canMove) {
+                moveDir = moveDirX;
+            } else {
+                // try move on y dir
+                var moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(
+                    playerPos,
+                    playerPos + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirZ,
+                    moveDistance);
+                if (canMove) {
+                    moveDir = moveDirZ;
+                } else {
+                    // totally can not move
+                }
+            }
+        }
+
+        if (canMove) {
+            playTransform.position += moveDir * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
     }
