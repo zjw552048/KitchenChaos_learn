e81e152740b6604d84fdfa28d44770730dee5118
zjw-pc <>
2023-07-28 13:13:07 +0800

    追加测试counter在已经存在kitchenObject时，父节点、位置切换至另一个counter

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -1,11 +1,45 @@
+using System;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
     [SerializeField] private Transform counterTopPoint;
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
 
+    private KitchenObject kitchenObject;
+
     public void Interact() {
-        Debug.Log("Interact with " + transform);
-        var kitchenObject = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
+        if (kitchenObject == null) {
+            var kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        } else {
+            Debug.Log("Interact with has KitchenObject counter");
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void SetKitChenObject(KitchenObject targetObject) {
+        kitchenObject = targetObject;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
+    }
+
+    [SerializeField] private ClearCounter otherClearCounter;
+    [SerializeField] private bool testing;
+
+    private void Update() {
+        if (testing && Input.GetKey(KeyCode.T)) {
+            if (HasKitchenObject()) {
+                kitchenObject.SetClearCounter(otherClearCounter);
+            }
+        }
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -3,7 +3,29 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour {
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
 
+    private ClearCounter counter;
+
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSo;
+    }
+
+    public void SetClearCounter(ClearCounter targetCounter) {
+        if (targetCounter.HasKitchenObject()) {
+            Debug.LogError("targetCounter already has a kitchenObject!");
+        }
+
+        if (counter != null) {
+            counter.SetKitChenObject(null);
+        }
+
+        counter = targetCounter;
+        transform.parent = counter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+        
+        counter.SetKitChenObject(this);
+    }
+
+    public ClearCounter GetClearCount() {
+        return counter;
     }
 }
