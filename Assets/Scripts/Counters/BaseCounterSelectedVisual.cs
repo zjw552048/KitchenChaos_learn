5c1ccad363a4a91f7c03838253bb03b766ca757a
zjw-pc <>
2023-07-28 20:24:02 +0800

    修改脚本名称

--- Assets/Scripts/Counters/BaseCounterSelectedVisual.cs
+++ Assets/Scripts/Counters/BaseCounterSelectedVisual.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SelectedCounterVistual : MonoBehaviour {
+public class BaseCounterSelectedVisual : MonoBehaviour {
 
     [SerializeField]private GameObject counterSelectedVisual;
     
