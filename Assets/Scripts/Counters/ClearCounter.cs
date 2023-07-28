ee4d1fe55f3fd8cc3ab8bf9c44cc900256683ba0
zjw-pc <>
2023-07-28 17:48:10 +0800

    整理player与ClearCounter、ContainerCounter的交互逻辑

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -1,7 +1,19 @@
-using UnityEngine;
-
 public class ClearCounter : BaseCounter {
     public override void Interact(Player player) {
-        Debug.Log("Interact with ClearCounter " + transform);
+        if (player.HasKitchenObject()) {
+            if (HasKitchenObject()) {
+                // player手持物体，counter被占用，无逻辑
+            } else {
+                // player手持物体，counter空闲，放下物体
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        } else {
+            if (HasKitchenObject()) {
+                // player未手持物体，counter被占用，拾取物体
+                GetKitchenObject().SetKitchenObjectParent(player);
+            } else {
+                // player未手持物体，counter空闲，无逻辑
+            }
+        }
     }
 }
--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -6,15 +6,19 @@ public class ContainerCounter : BaseCounter {
     [SerializeField] private SpriteRenderer kitchenObjectIcon;
 
     public event Action PlayerHoldKitchenObjectAction;
-    
+
     public void Start() {
         kitchenObjectIcon.sprite = kitchenObjectSo.icon;
     }
 
     public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            return;
+        }
+
         var kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab);
         kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        
+
         PlayerHoldKitchenObjectAction?.Invoke();
     }
 }
