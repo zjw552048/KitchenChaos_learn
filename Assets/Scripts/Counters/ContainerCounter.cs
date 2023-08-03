349cedfa9b6b7c2122522ae90df61ad17d83225f
zjw-pc <>
2023-08-03 18:23:24 +0800

    同步ContainerCounter打开动画

--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Netcode;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter {
@@ -18,6 +19,16 @@ public class ContainerCounter : BaseCounter {
 
         KitchenObject.SpawnKitchenObject(kitchenObjectSo, player);
 
+        PlayerHoldKitchenObjectActionServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void PlayerHoldKitchenObjectActionServerRpc() {
+        PlayerHoldKitchenObjectActionClientRpc();
+    }
+    
+    [ClientRpc]
+    private void PlayerHoldKitchenObjectActionClientRpc() {
         PlayerHoldKitchenObjectAction?.Invoke();
     }
 
