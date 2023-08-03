84eac82bcba048f54c1097cb816c2668de6bdc55
zjw-pc <>
2023-08-03 15:15:38 +0800

    修改DliverManager，ServerAuth方式同步生成的reciple；ClientAuth方式同步deliveryResult

--- Assets/Scripts/Manager/DeliveryManager.cs
+++ Assets/Scripts/Manager/DeliveryManager.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class DeliveryManager : MonoBehaviour {
+public class DeliveryManager : NetworkBehaviour {
     public static DeliveryManager Instance { get; private set; }
 
     [SerializeField] private RecipeListSo recipeListSo;
@@ -27,6 +28,10 @@ public class DeliveryManager : MonoBehaviour {
     }
 
     private void Update() {
+        if (!IsServer) {
+            return;
+        }
+
         if (!MainGameManager.Instance.IsGamePlayingState()) {
             return;
         }
@@ -42,7 +47,13 @@ public class DeliveryManager : MonoBehaviour {
             return;
         }
 
-        var newRecipeSo = recipeListSo.recipeSos[Random.Range(0, recipeListSo.recipeSos.Length)];
+        var recipeIndex = Random.Range(0, recipeListSo.recipeSos.Length);
+        AddWaitingRecipeClientRpc(recipeIndex);
+    }
+
+    [ClientRpc]
+    private void AddWaitingRecipeClientRpc(int recipeIndex) {
+        var newRecipeSo = recipeListSo.recipeSos[recipeIndex];
         waitingRecipeSos.Add(newRecipeSo);
         SpawnRecipeAction?.Invoke();
         Debug.Log("newRecipeSo: " + newRecipeSo.recipeName);
@@ -76,20 +87,40 @@ public class DeliveryManager : MonoBehaviour {
             }
 
             if (matchRecipe) {
-                Debug.Log("player delivery the correct recipe, which index: " + i + ", recipeName: " +
-                          waitingRecipeSos[i].recipeName);
-                recipeSuccessfulCount++;
-                waitingRecipeSos.RemoveAt(i);
-                CompleteRecipeAction?.Invoke();
-                DeliverySuccessAction?.Invoke();
-                SoundManager.Instance.PlayDeliverySuccess(plateKitchenObject.transform.position);
+                DeliverySuccessServerRpc(i, plateKitchenObject.transform.position);
                 return;
             }
         }
 
+        DeliveryFailServerRpc(plateKitchenObject.transform.position);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DeliverySuccessServerRpc(int recipeIndex, Vector3 platePos) {
+        DeliverySuccessClientRpc(recipeIndex, platePos);
+    }
+
+    [ClientRpc]
+    private void DeliverySuccessClientRpc(int recipeIndex, Vector3 platePos) {
+        Debug.Log("player delivery the correct recipe, which index: " + recipeIndex + ", recipeName: " +
+                  waitingRecipeSos[recipeIndex].recipeName);
+        recipeSuccessfulCount++;
+        waitingRecipeSos.RemoveAt(recipeIndex);
+        CompleteRecipeAction?.Invoke();
+        DeliverySuccessAction?.Invoke();
+        SoundManager.Instance.PlayDeliverySuccess(platePos);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DeliveryFailServerRpc(Vector3 platePos) {
+        DeliveryFailClientRpc(platePos);
+    }
+
+    [ClientRpc]
+    private void DeliveryFailClientRpc(Vector3 platePos) {
         Debug.LogWarning("player delivery the wrong recipe!");
         DeliveryFailAction?.Invoke();
-        SoundManager.Instance.PlayDeliveryFail(plateKitchenObject.transform.position);
+        SoundManager.Instance.PlayDeliveryFail(platePos);
     }
 
     public List<RecipeSo> GetWaitingRecipeSos() {
@@ -99,5 +130,4 @@ public class DeliveryManager : MonoBehaviour {
     public int GetRecipeSuccessfulCount() {
         return recipeSuccessfulCount;
     }
-
 }
