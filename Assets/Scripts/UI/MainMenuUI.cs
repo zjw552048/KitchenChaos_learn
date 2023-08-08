531a8a1ff6843208ea02480d1a13021773cb1baf
zjw-pc <>
2023-08-08 13:17:24 +0800

    追加补丁逻辑修复首个场景UI元素位置异常

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +6,7 @@ public class MainMenuUI : MonoBehaviour {
     [SerializeField] private Button multiplayerBtn;
     [SerializeField] private Button singlePlayerBtn;
     [SerializeField] private Button quitBtn;
+    [SerializeField] private GameObject logo;
 
     private void Start() {
         Time.timeScale = 1f;
@@ -19,4 +21,37 @@ public class MainMenuUI : MonoBehaviour {
 
         quitBtn.onClick.AddListener(Application.Quit);
     }
+
+    #region 补丁逻辑，未知原因造成首次进入场景时，UI元素的高度不正确，位置异常。
+
+    private bool hasFixUiPosition;
+
+    private void TryFixUIPosition() {
+        if (hasFixUiPosition) {
+            return;
+        }
+
+        var canvasTransform = (RectTransform) transform.parent;
+        if (!(Math.Abs(canvasTransform.rect.height - 1080) < 1)) {
+            return;
+        }
+
+        Debug.LogWarning("success fix ui position!");
+        hasFixUiPosition = true;
+
+        var multiplayerBtnTransform = (RectTransform) multiplayerBtn.transform;
+        multiplayerBtnTransform.anchoredPosition = new Vector2(105, 430);
+        var singlePlayerBtnTransform = (RectTransform) singlePlayerBtn.transform;
+        singlePlayerBtnTransform.anchoredPosition = new Vector2(105, 240);
+        var quitBtnTransform = (RectTransform) quitBtn.transform;
+        quitBtnTransform.anchoredPosition = new Vector2(105, 70);
+        var logoTransform = (RectTransform) logo.transform;
+        logoTransform.anchoredPosition = new Vector2(0, -50);
+    }
+
+    private void Update() {
+        TryFixUIPosition();
+    }
+
+    #endregion
 }
